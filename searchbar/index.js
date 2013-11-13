f8cf3c84941d93fbd2429268c58362197259d39d
wilhelmson <wilhelmson@el-xps.(none)>
2013-11-13 11:40:12 -0800

    setting up the searchbar

---
+++ searchbar/index.js
@@ -0,0 +1,40 @@
+//don't forget to npm install com-delegate;
+var Delegate = require('dom-delegate'),
+	EventEmitter = require('events').EventEmitter,
+	reqwest = require('reqwest'),
+	apiKey = '934910a2c34c06b6b167e1589069c274',
+	baseUrl = "http://api.flickr.com/services/rest/?&api_key=" + apiKey + "&format=json&nojsoncallback=1&api_cluster=";
+
+function SearchBar(element){
+	EventEmitter.call(this);
+	this.element = element;
+	this.term = '';
+	this.delegate = new Delegate(this.element);
+	this.render();
+	this.delegate.on('blur', '.searchTerm', this.onTermEntered.bind(this));
+	this.delegate.on('click', '.searchButton', this.onSearchClicked.bind(this));
+}
+
+SearchBar.prototype.render = function(){
+	//call handlebars to get the template
+	//set initial value to this.searchTerm;
+}
+
+SearchBar.prototype.onTermEntered(e){
+	var term = e.target.value;
+	this.term = term || '';
+}
+
+SearchBar.prototype.onSearchClicked = function(e){
+	if(this.term){
+		//update navigation using location-bar;
+		url = baseUrl + "&method=flickr.tags.getClusterPhotos&tag=" + this.term;		
+		reqwest({
+			url: url,
+			method: "get",
+			type: "json",
+			crossOrigin: true,
+			success: this.emit('searchReturned');		
+		});
+	}
+}
